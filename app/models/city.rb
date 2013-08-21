b49ab2b813422e81a4a4736f2a20e3cd50dc868d
Kathleen Baynes <ktbaynes@gmail.com>
2013-08-20 22:05:09 -0400

    adding testing to base stats and alignment, starting on size

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,7 @@
 class City < ActiveRecord::Base
 
+  @@city_data_hash ||= Hash.from_xml(Nokogiri::XML(File.read(Figaro.env.xml_base + Figaro.env.city_data_file)).to_s)
+
   def self.build_new_city
     c = City.new
     c.name = CityName.generate_city_name
@@ -13,23 +15,40 @@ class City < ActiveRecord::Base
     c
   end
 
+  #TODO modify city age by size_modifier
   def self.generate_city_age
     rand(Figaro.env.max_city_age.to_i).to_s
   end
 
-  def self.generate_base_stats
+  def self.generate_base_stats(stat_array=nil)
     base_attr = [ "education", "authority", "magic", "military", "tolerance", "economy"]
-    #taken from the CityGenerator.pm ... this generates values from -4 to 5 ... WHY?
-    Hash[base_attr.map { |k, v| [k.to_sym, [-5, [5,(CityGenTools.d(11)-5)].min].max]}]
+    if stat_array
+      raise ("Wrong number of elements passed, expected #{base_attr.length} received #{stat_array.length}") unless stat_array.length == base_attr.length
+      if stat_array.reject! {|stat| (stat > 5 || stat < -4) }
+        raise ("Invalid value passed in stat array: #{stat_array}")
+      end
+      
+      Hash[base_attr.zip(stat_array)]
+    else
+      #taken from the CityGenerator.pm ... this generates values from -4 to 5 ... WHY?
+      Hash[base_attr.map { |k, v| [k.to_sym, [-5, [5,(CityGenTools.d(11)-5)].min].max]}]
+    end
   end
 
-  def self.generate_alignment
-    #taken from the CityGenerator.pm ... this generates values from -4 to 5 ... WHY?
-    Hash[[:moral, :order].map { |k, v| [k.to_sym, CityGenTools.d(100)]}]
+  def self.generate_alignment(stat_array=nil)
+    alignment_attr = ["moral", "order"]
+    if stat_array
+      raise ("Wrong number of elements passed, expected #{alignment_attr.length} received #{stat_array.length}") unless stat_array.length == alignment_attr.length
+      if stat_array.reject! {|stat| (stat < 1 || stat > 100) }
+        raise ("Invalid value passed in stat array: #{stat_array}")
+      end
+    end
+    Hash[alignment_attr.map { |k, v| [k.to_sym, CityGenTools.d(100)]}]
   end
 
   def self.generate_size
-
+    size_hash = CityGenTools.get_hash_value(@@city_data_hash["data"]["citysize"], "city")
+    puts size_hash
   end
 
 end
--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,8 +1,69 @@
 require 'spec_helper'
 
 describe City do
-  it "should return a valid city age" do
-    City.generate_city_age.to_i.should <= Figaro.env.max_city_age.to_i
-    City.generate_city_age.to_i.should >= 0
+  context 'when generating city age' do
+    it "should return a valid city age" do
+      age = City.generate_city_age.to_i
+      if age > Figaro.env.max_city_age.to_i || age <= 0
+        found "Found an invalid city age: #{age}"
+      end
+    end
+  end
+
+  context 'when generating city base stats' do
+    it "should generate valid stats without input" do
+      hash = City.generate_base_stats
+      if hash.reject! {|k,v| v > 5 || v < -4}
+        fail "Found an invalid value in generated stats: #{hash}"
+      end
+    end
+
+    it "should generate valid stats with valid inputs" do
+      stats = [-4,-3,-4,5,3,0]
+      hash = City.generate_base_stats(stats)
+      if hash.reject! {|k,v| v > 5 || v < -4 }
+        fail "Found an invalid value in input stats: #{stats}"
+      end
+    end
+
+    it "should fail to generate stats with invalid inputs" do
+      stats = [-67,-3,-4,5,3,0]
+        expect{City.generate_base_stats(stats)}.to raise_error
+    end
+
+    it "should reject requests with the wrong number of inputs" do
+      stats = [1,1,1]
+      expect{City.generate_base_stats(stats)}.to raise_error
+      stats = [1,1,1,1,1,1,1,1,1]
+      expect{City.generate_base_stats(stats)}.to raise_error
+    end
+  end
+
+  context 'when generating city alignment' do
+
+    it "should generate valid stats without input" do
+      hash = City.generate_alignment
+      if hash.reject! {|k,v| v < 1 || v > 100}
+        fail "Found an invalid value in generated stats: #{hash}"
+      end
+    end
+
+    it "should fail to generate stats with invalid inputs" do
+      stats = [1, 100]
+      hash = City.generate_alignment(stats)
+      if hash.reject! {|k,v| v < 1 || v > 100 }
+        fail "Found an invalid value in input stats: #{stats}"
+      end
+    end
+    
+    it "should fail to generate stats with invalid inputs" do
+      stats = [-500, 200]
+        expect{City.generate_alignment(stats)}.to raise_error
+    end
+
+    it "should reject requests with the wrong number of inputs" do
+      stats = [1,1,1,1]
+        expect{City.generate_alignment(stats)}.to raise_error
+    end
   end
 end
