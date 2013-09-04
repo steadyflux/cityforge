38b13c3dcd37ce635af3130b637f563508741a41
Kathleen Baynes <ktbaynes@gmail.com>
2013-09-03 21:47:46 -0400

    adding nokogiri to load_config

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
 
 CITY_DATA_HASH = Hash.from_xml(Nokogiri::XML(File.read(APP_CONFIG['xml_base'] + "citydata.xml")).to_s)
